45352f986d224c498a0bbe512efbbe30b3f6839b
nick <n.m.98@gmx.de>
2020-04-16 11:06:10 +0200

    update

--- app/models.py
+++ app/models.py
@@ -1,15 +1,26 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
-    id = db. Column (db.Integer, primary_key=True)
+
+class User(UserMixin, db.Model):
+    id = db.Column (db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     useremail = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    event_id = db.Column(db.String(10))
     events = db.relationship('Event', backref='host', lazy='dynamic')
-    
+    email = db.Column(db.String(10))
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,3 +53,8 @@ class Message(db.Model):
 
     def __repr__(self):
          return '<Message {}>'.format(self.messagesubject)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+from flask_login import login_required
+from app import db
+
 
 @app.route ('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Alaida'}
     events = [
         {
             'event_name' : 'Watte-Party',
@@ -18,12 +23,47 @@ def index():
         }
 
     ]
-    return render_template('index.html', title='Homy', user=user, events=events)
+    return render_template('index.html', title='Homy', events=events)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form= LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}' .format(form.username.data, form.remember_me.data))
-        return redirect (url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect (url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user= User(username=form.username.data, email=form.useremail.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    events=[
+        {'host': user, 'eventname': 'Testevent1'},
+        {'host': user, 'eventname': 'Testevent1'}
+     ]
+     return render_template('user.html', user=user, events=events)
