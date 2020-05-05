69657132efbe698dfbf5518430f77d96c7730e2d
nick <n.m.98@gmx.de>
2020-05-05 18:20:41 +0200

    design

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail, Message
+from flask_bootstrap import Bootstrap
+
 
 
 
@@ -13,7 +15,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-mail= Mail(app)
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
--- config.py
+++ config.py
@@ -1,14 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '015777233653'
+    SECRET_KEY = os.getenv('SECRET_KEY') 
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://nick:@localhost:5432/whoshere'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_KEY') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.sendgrid.net'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'apikey'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = 'whoshere.berlin@gmail.com'
